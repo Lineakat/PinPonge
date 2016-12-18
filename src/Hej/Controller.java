5fff72708621cdb70b67c3bb6800cb81a635e41f
Lineakat <linea.w@hotmail.com>
2016-12-18 18:48:35 +0100

    har lavet noget på ball

---
+++ src/Hej/Controller.java
@@ -0,0 +1,17 @@
+package Hej;
+
+/**
+ * Created by Linea on 18/12/2016.
+ */
+public class Controller {
+
+    //Constructor
+    public Controller(){
+
+    }
+
+    public static void main(String[] args){
+        System.out.println("HEJ PHILIP du er dejlig!");
+        System.out.println("HEJ PHILIP jeg elsker dig!");
+    }
+}
